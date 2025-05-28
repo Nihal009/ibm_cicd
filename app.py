963baeee558b5be26414b07abed456b625f5eb40
Nihal009 <nihalcrixcrox56@gmail.com>
2025-05-28 21:56:11 +0530

    5555

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return """testststststt
-    ashdgfhgasdjgfashgdfgasdjfgajsdfagsdfa
-    asjdfjasdjasd"""
+    return """121211"""
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
