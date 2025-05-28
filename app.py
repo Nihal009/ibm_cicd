9f86388aae4f40b57c83c2bd8462cf9fc13c59f0
Nihal009 <nihalcrixcrox56@gmail.com>
2025-05-28 22:36:39 +0530

    3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return """latest update 1122121"""
+    return """latest update 3333331"""
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
