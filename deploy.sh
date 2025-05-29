e27683adbfa770af5925818474c645ecff711ffc
Nihal009 <nihalcrixcrox56@gmail.com>
2025-05-29 21:32:38 +0530

    000000

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "[+] Pulling latest Docker image"
-docker pull nihal009/flask-app:latest
+docker pull nihal009/flask-app:v1
 
 echo "[+] Redeploying to Kubernetes"
 kubectl apply -f k8s/deployment.yml
