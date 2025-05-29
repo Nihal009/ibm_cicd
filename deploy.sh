73d10ca3f4887db9ee9d7464692790a295e382ad
Nihal009 <nihalcrixcrox56@gmail.com>
2025-05-29 10:00:31 +0530

    changes in pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "[+] Pulling latest Docker image"
-docker pull nihal009/flask-app:v1
+docker pull nihal009/flask-app:latest
 
 echo "[+] Redeploying to Kubernetes"
 kubectl apply -f k8s/deployment.yml
+
+echo "restart..."
+kubectl rollout restart deployment flask-app
 
 echo "[+] Deployment complete"
