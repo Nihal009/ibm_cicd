24ae14091d0951d55b1ce7d66275125e3cb22c6b
Nihal009 <nihalcrixcrox56@gmail.com>
2025-05-30 11:10:53 +0530

    upadsd

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ app = Flask(__name__)
 def hello_world():
     return """<html>
 <head>
-  <title>Inline Styled Page</title>
+  <title>cicd</title>
 </head>
 <body style="background-color: #f0f0f0; font-family: Arial, sans-serif; text-align: center;">
 
-  <h1 style="color: #2c3e50; margin-top: 50px;">Welcome to My Page</h1>
+  <h1 style="color: #2c3e50; margin-top: 50px;">k8 cicd</h1>
 
   <p style="color: #555; font-size: 18px;">
-    This is a simple HTML page with inline styling.
+    Kuberenets cicd pipeline test.
   </p>
 
   <button style="padding: 10px 20px; background-color: #3498db; color: white; border: none; border-radius: 5px; cursor: pointer;">
