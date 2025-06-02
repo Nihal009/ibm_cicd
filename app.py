337c5aff710d46258e9a6d24492417d4b999a4e2
Nihal009 <nihalcrixcrox56@gmail.com>
2025-06-02 19:27:23 +0530

    updated flask app

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def hello_world():
   <h1 style="color: #2c3e50; margin-top: 50px;">k8 cicd</h1>
 
   <p style="color: #555; font-size: 18px;">
-    changes made.
+    changes made again.
   </p>
 
   <button style="padding: 10px 20px; background-color: #3498db; color: white; border: none; border-radius: 5px; cursor: pointer;">
